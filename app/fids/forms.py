208e692792e2311647c99d6d04896c7ae24d8927
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-10-30 15:55:20 +0530

    Added add feature

---
+++ app/fids/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateTimeField
+from wtforms.validators import DataRequired, ValidationError
+
+from app.models import Flight
+
+class AddFlightForm(FlaskForm):
+    dep_time = DateTimeField("Departs at")
+    flight_code = StringField("Flight #", validators=[DataRequired()])
+    dest = StringField("Destination")
+    status = StringField("Status")
+    submit = SubmitField("Submit")
+    
+    def validate_flight_code(self, val):
+        if Flight.query.filter_by(flight_code = val).first():
+            raise ValidationError("flight already existent")
--- app/fids/routes.py
+++ app/fids/routes.py
@@ -1,25 +1,46 @@
-from flask import render_template
+import datetime
+
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_required
 
 from app import db
 from app.models import Flight
 import app.fids.roles as permissions
+from app.fids.forms import AddFlightForm
 
+@login_required
 @permissions.list_flights.require()
 def list_():
     f = Flight.query.all()
     return render_template("fids/list.html", title="Flight list", data=f)
 
 
+@login_required
 @permissions.add_flights.require()
 def add():
-    pass
+    form = AddFlightForm()
+    if form.validate_on_submit():
+        flight1 = Flight()
+        flight1.dep_time = form.dep_time.data
+        flight1.flight_code = form.flight_code.data
+        flight1.dest = form.dest.data
+        flight1.status = form.status.data
+        db.session.add(flight1)
+        db.session.commit()
+        flash("Success")
+        
+        return redirect(url_for("fids.list_"))
+    
+    return render_template("fids/edit.html", title="Add Flight", form=form)
 
 
+@login_required
 @permissions.edit_flights.require()
 def edit(id):
     pass
 
 
+@login_required
 @permissions.delete_flights.require()
 def delete(id):
     pass
